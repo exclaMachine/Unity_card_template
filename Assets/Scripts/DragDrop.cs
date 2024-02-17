20cd3662afc11c1dab4f6ddedb211de97b431dc0
Ty Rickers <ty.rickers@gmail.com>
2024-02-17 11:42:19 -0700

    started implementation of drag script

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool bIsDragging = false;
+    private Vector2 startPosition;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (bIsDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    public void StartDrag()
+    {
+        startPosition = transform.position;
+        bIsDragging = true;
     }
 }
