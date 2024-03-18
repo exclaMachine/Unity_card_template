bc5e8345f6328e2c9e19d838da56d28a47882d3a
Ty Rickers <ty.rickers@gmail.com>
2024-03-18 05:25:23 -0600

    added to onhover enter

--- Assets/Scripts/CardZoom.cs
+++ Assets/Scripts/CardZoom.cs
@@ -15,6 +15,7 @@ public class CardZoom : MonoBehaviour
     public void OnHoverEnter()
     {
         ZoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x, Input.mousePosition.y), Quaternion.identity);
+        ZoomCard.transform.SetParent(Canvas.transform, false);
     }
 
     public void OnHoverExit()
